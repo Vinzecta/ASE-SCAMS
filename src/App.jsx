9d90324f6ed445c4e2fc7b9b20edabd134895e81
Lmao1232j <vinhtran23042004@gmail.com>
2025-11-27 14:56:26 +0700

    Finish ui for authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import RoomBooking from "./features/RoomBooking/RoomBooking";
+import Login from "./features/Authentication/Login";
+import SignUp from "./features/Authentication/SignUp";
+import ForgotPassword from "./features/Authentication/ResetPassword";
 import "./App.css";
 
 const Dashboard = () => (
@@ -12,7 +15,9 @@ function App() {
     return (
         <Routes>
             <Route path="/" element={<Dashboard />} />
-
+            <Route path="/log-in" element={<Login />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/book-room" element={<RoomBooking />} />
         </Routes>
     );
